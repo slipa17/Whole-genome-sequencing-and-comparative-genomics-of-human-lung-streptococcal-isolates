05f67085ff99d52ca8564cc289e3a5bfbd20b274
Sudip Das <55438047+sudu87@users.noreply.github.com>
2023-07-21 21:56:15 +0200

    Update prokka.sh

--- prokka.sh
+++ prokka.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the path to the folder containing your FASTA files
-fasta_folder="/Users/sdas/Documents/Research/Genomes_resource/abricate/genomes_abricate"
+fasta_folder="$PATH/to/genome FASTA files"
 
 # Set the output folder where Prokka results will be saved
-output_folder="/Users/sdas/Documents/Research/Genomes_resource/prokka_strep_all_ref"
+output_folder="$PATH/to/output folder"
 
 # Loop through each FASTA file in the folder and run Prokka
 for fasta_file in $fasta_folder/*.fna; do
