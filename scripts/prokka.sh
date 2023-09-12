952b810038ca38a24e69e99d8063a9a363ba1951
Sudip Das <55438047+sudu87@users.noreply.github.com>
2023-09-12 11:28:48 +0200

    Update prokka.sh

--- scripts/prokka.sh
+++ scripts/prokka.sh
@@ -9,20 +9,17 @@ if [ ! -d "$fasta_folder" ]; then
     exit 1
 fi
 
-# Set the output folder where Prokka results will be saved
-output_folder="$fasta_folder/prokka_output"
-
-# Create the output folder if it doesn't exist
-mkdir -p "$output_folder"
-
 # Find all .fna files recursively under the fasta_folder and process them
 find "$fasta_folder" -type f -name "*.fna" | while read -r fasta_file; do
     # Get the file name without extension to use as the Prokka output name
     output_name=$(basename "$fasta_file" .fna)
 
-    # Create a subdirectory in the same folder as the .fna file for Prokka output
-    mkdir -p "$(dirname "$fasta_file")/$output_name"
+    # Create a subdirectory within the same folder as the .fna file for Prokka output
+    output_folder="$(dirname "$fasta_file")/$output_name"
+
+    # Create the output folder if it doesn't exist
+    mkdir -p "$output_folder"
 
     # Run Prokka on the current FASTA file
-    prokka --outdir "$(dirname "$fasta_file")/$output_name" --prefix "$output_name" "$fasta_file"
+    prokka --outdir "$output_folder" --prefix "$output_name" "$fasta_file"
 done
