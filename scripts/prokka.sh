4e14c7203d605010cd1d4f598bf1685fc8483b4d
Sudip Das <55438047+sudu87@users.noreply.github.com>
2023-09-12 11:13:55 +0200

    Update prokka.sh

--- scripts/prokka.sh
+++ scripts/prokka.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Set the path to the folder containing your FASTA files
-fasta_folder="$PATH/to/genome FASTA files"
+# Set the path to the top-level folder containing your FASTA files and subfolders
+fasta_folder="$PATH/to/top-level folder"
 
 # Set the output folder where Prokka results will be saved
 output_folder="$PATH/to/output folder"
 
-# Loop through each FASTA file in the folder and run Prokka
-for fasta_file in $fasta_folder/*.fna; do
+# Find all .fna files recursively under the fasta_folder and process them
+find "$fasta_folder" -type f -name "*.fna" | while read -r fasta_file; do
     # Get the file name without extension to use as the Prokka output name
-    output_name=$(basename "$fasta_file")
+    output_name=$(basename "$fasta_file" .fna)
+
+    # Create a subdirectory in the output folder with the same name as the output_name
+    mkdir -p "$output_folder/$output_name"
 
     # Run Prokka on the current FASTA file
     prokka --outdir "$output_folder/$output_name" --prefix "$output_name" "$fasta_file"
