79a304ad3ecd7cd3726beb44988cd6ad7561f2be
Sudip Das <55438047+sudu87@users.noreply.github.com>
2023-09-12 11:42:49 +0200

    Update prokka.sh

--- scripts/prokka.sh
+++ scripts/prokka.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Prompt the user for the path to the top-level folder containing .fna files
-read -p "Enter the path to the top-level folder containing .fna files: " fasta_folder
+# Ask the user for the path to the folder containing .fna files
+read -p "Enter the path to the folder containing .fna files: " input_folder
 
-# Ensure the specified folder exists
-if [ ! -d "$fasta_folder" ]; then
-    echo "Error: The specified folder does not exist."
+# Check if the specified input folder exists
+if [ ! -d "$input_folder" ]; then
+    echo "Error: The specified input folder does not exist."
     exit 1
 fi
 
-# Prompt the user for the path to the output folder
+# Ask the user for the path to the output folder
 read -p "Enter the path to the output folder: " output_folder
 
 # Create the output folder if it doesn't exist
 mkdir -p "$output_folder"
 
-# Find all .fna files recursively under the fasta_folder and process them
-find "$fasta_folder" -type f -name "*.fna" | while read -r fasta_file; do
-    # Get the file name without extension to use as the Prokka output name
-    output_name=$(basename "$fasta_file" .fna)
+# Loop through each .fna file in the input folder and run Prokka
+for fna_file in "$input_folder"/*.fna; do
+    # Get the base name of the file (without the path or extension)
+    base_name=$(basename "$fna_file" .fna)
 
-    # Create a subdirectory within the output folder for Prokka output
-    output_subfolder="$output_folder/$output_name"
-    mkdir -p "$output_subfolder"
-
-    # Run Prokka on the current FASTA file
-    prokka --outdir "$output_subfolder" --prefix "$output_name" "$fasta_file"
+    # Run Prokka on the current .fna file
+    prokka --outdir "$output_folder/$base_name" --prefix "$base_name" "$fna_file"
 done
+
+echo "Prokka annotation of .fna files in '$input_folder' is complete. Results saved in '$output_folder'."
