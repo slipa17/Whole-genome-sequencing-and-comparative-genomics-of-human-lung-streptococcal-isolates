9b5174bf496d05dde5ecfc36c5ac7d143726bbf5
Sudip Das <55438047+sudu87@users.noreply.github.com>
2023-09-13 15:35:45 +0200

    Update concatenate.sh

--- scripts/concatenate.sh
+++ scripts/concatenate.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Ask the user for the input directory
+read -p "Enter the input directory path: " input_dir
+
+# Ask the user for the output filename
 read -p "Enter the output filename: " output_file
 
 # Initialize an empty array to store input filenames
 input_files=()
 
-# Collect all fasta files in the current directory and add them to the input_files array
-for file in *.fasta; do
+# Collect all fasta files in the specified input directory and add them to the input_files array
+for file in "$input_dir"/*.faa; do
     if [ -f "$file" ]; then
         input_files+=("$file")
     fi
@@ -14,7 +18,7 @@ done
 
 # Check if there are any fasta files to concatenate
 if [ ${#input_files[@]} -eq 0 ]; then
-    echo "No fasta files found in the current directory."
+    echo "No fasta files found in the specified input directory."
     exit 1
 fi
 
@@ -23,3 +27,5 @@ for file_name in "${input_files[@]}"; do
     cat "$file_name" >> "$output_file"
     echo >> "$output_file"
 done
+
+echo "Fasta files from $input_dir successfully concatenated to $output_file."
